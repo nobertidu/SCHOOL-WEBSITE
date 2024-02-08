7675bbce44d3449ade54b9b7c76836aaba3535cd
Nicholas Nobert <nicholasidu12@gmail.com>
2024-02-08 23:15:12 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+//change navbar scroll style
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle(
+        'window-scroll', window.scrollY > 0
+    )
+} )
+
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //CHANGE ICON
+        const icon = faq.querySelector('.faq__icon i')
+        if(icon.className === 'fa-solid fa-circle-plus' ) {
+            icon.className = "fa-solid fa-circle-minus"
+            
+        }else{
+            icon.className = 'fa-solid fa-circle-plus';
+        }
+    })
+})
+
+
+//show/hide nav menu
+
+const menu = document.querySelector('.nav__menu')
+const menuBtn = document.querySelector('#open-menu-btn')
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+     menu.style.display = 'flex';
+     closeBtn.style.display = 'inline-block';
+     menuBtn.style.display = 'none';
+ })
+
+
+
+ //closebtn
+ const closeNav = () => {
+     menu.style.display = 'none';
+     closeBtn.style.display = 'none'
+     menuBtn.style.display = 'inline-block';
+ }
+
+closeBtn.addEventListener('click', closeNav)
